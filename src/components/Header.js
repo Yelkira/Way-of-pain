bdb7233a30b1ecadf4a3d358973c4918be07a8c1
Yelkira <penda.fu@gmail.com>
2022-10-08 23:54:05 +0300

    kek

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (<header className="header">
+        <img alt="siso"
+             src="https://static8.depositphotos.com/1378583/1010/i/600/depositphotos_10108949-stock-photo-blue-flame-logo.jpg"/>
+    </header>);
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img alt="pup"
+                 src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Ffar-cry-5%2Fhome%2FFC5_01_Banner-1200x200-c79e652045d64313f111e67ac5d3aeb5e8c32b17.jpg"/>
+        </div>
+        <div>
+            <img alt="pup2"
+                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkoKq1Cw65jQDOf767nYXrnmK-TIWRn2-W7dMWq3q6&s"/>
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+    </div>
+}
+export default Profile;
