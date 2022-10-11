51aefbccf5079117d675918bed5e759532784726
Yelkira <penda.fu@gmail.com>
2022-10-11 23:56:45 +0300

    somethings new?

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import "./Profile.module.css";
 import s from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img alt="pup"
                  src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Ffar-cry-5%2Fhome%2FFC5_01_Banner-1200x200-c79e652045d64313f111e67ac5d3aeb5e8c32b17.jpg"/>
