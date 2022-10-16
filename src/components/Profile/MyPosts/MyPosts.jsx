4087c4d622ec213b12e8e0e20f0a165c8338475f
Yelkira <penda.fu@gmail.com>
2022-10-16 21:41:44 +0300

    26

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+
+
+const MyPosts = (props) => {
+
     let posts = [
         {id: 1, message: "Hi, how are you?", likesCount: 12},
         {id: 2, message: "It's my first post", likesCount: 11},
