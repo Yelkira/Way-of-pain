ed2d2549e396c6eb926d1d04bba2f03e801c7ad8
Yelkira <penda.fu@gmail.com>
2022-10-16 23:36:26 +0300

    28 bll ui

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,27 +5,10 @@ import Message from "./Message/Message";
 
 
 const Dialogs = (props) => {
-
-    let dialogs = [
-        {id: 1, name: "Dimych"},
-        {id: 2, name: "Andrey"},
-        {id: 3, name: "Sveta"},
-        {id: 4, name: "Sasha"},
-        {id: 5, name: "Valery"},
-        {id: 6, name: "Victor"}
-    ]
-    let messages = [
-        {id: 1, message: "Hi"},
-        {id: 2, message: "How is your it-kamasutra"},
-        {id: 3, message: "Yo?"},
-        {id: 4, message: "Yo"},
-        {id: 5, message: "Yo"},
-        {id: 6, message: "Yo"}
-    ]
-    let dialogsElements = dialogs
-        .map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = messages
-        .map(m => <Message message={m.message}/>);
+    let dialogsElements =
+        props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements =
+        props.messages.map(m => <Message message={m.message}/>);
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
+let posts = [
+    {id: 1, message: "Hi, how are you?", likesCount: 12},
+    {id: 2, message: "It's my first post", likesCount: 11},
+]
+let dialogs = [
+    {id: 1, name: "Dimych"},
+    {id: 2, name: "Andrey"},
+    {id: 3, name: "Sveta"},
+    {id: 4, name: "Sasha"},
+    {id: 5, name: "Valery"},
+    {id: 6, name: "Victor"}
+]
+let messages = [
+    {id: 1, message: "Hi"},
+    {id: 2, message: "How is your it-kamasutra"},
+    {id: 3, message: "Yo?"},
+    {id: 4, message: "Yo"},
+    {id: 5, message: "Yo"},
+    {id: 6, message: "Yo"}
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>
 );
 
