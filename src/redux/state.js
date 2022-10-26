e349dadb3ddf60a39d8d532d18252befc4823169
Yelkira <penda.fu@gmail.com>
2022-10-26 21:12:51 +0300

    action creator, action type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
     _state: {
         profilePage: {
@@ -37,7 +40,7 @@ let store = {
         this._callSubscriber = observer; //observer наблюдатель- паттерн...
     },
     dispatch(action) {           //(type: "ADD-POST")
-        if (action.type === "ADD-POST") {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -47,12 +50,20 @@ let store = {
             this._state.profilePage.newPostText = " ";
             // updateNewPostText(" ");
             this._callSubscriber(this._state);
-        } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
 }
+export const addPostActionCreator = () => ({
+        type: ADD_POST
+    })
+ export const updateNewPostTextActionCreator = (text) => ({
+    type: UPDATE_NEW_POST_TEXT, newText: text
+})
+
+
 
 
 export default store;
