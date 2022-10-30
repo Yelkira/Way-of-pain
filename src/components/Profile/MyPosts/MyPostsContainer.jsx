ec72b7f1ee74d017e26928b19a195cc7cd8ef9b0
Yelkira <penda.fu@gmail.com>
2022-10-30 15:01:27 +0300

    45 react-redux, connect

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
+import {connect} from "react-redux";
 
-
-const MyPostsContainer = (props) => {
-    let state = props.store.getState().profilePage;
-    let addPost = () => {
-        props.store.dispatch(addPostActionCreator());
+let mapStateToProps = (state) => {
+    return {
+        profilePage: state.profilePage
     }
-
-    let onPostChange = (text) => {
-        let action = updateNewPostTextActionCreator(text);
-        props.store.dispatch(action);
-    }
-    return (<MyPosts updateNewPostText={onPostChange}
-                     addPost={addPost}
-                     profilePage={state}/>)
 }
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (text) => {
+            dispatch(updateNewPostTextActionCreator(text))
+        },
+        addPost: () => {
+            dispatch(addPostActionCreator())
+        }
+
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 export default MyPostsContainer;
--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import store from "./redux/redux-store";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-export let rerenderEntireTree = (state) => {
+export let rerenderEntireTree = () => {
     root.render(
         <React.StrictMode>
-            <App state={state}
-                 dispatch={store.dispatch.bind(store)}
-                 store={store}/>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </React.StrictMode>
     );
 };
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-store.subscribe(()=>{
-    let state = store.getState();
-    rerenderEntireTree(state);
+store.subscribe(() => {
+    rerenderEntireTree();
 });
