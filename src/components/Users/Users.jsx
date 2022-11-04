dcb0de0236122e07ff899f533d935496305fcf58
Yelkira <penda.fu@gmail.com>
2022-11-04 23:27:13 +0300

    51 axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,17 @@
 import React from "react";
 import s from "./Users.module.css"
-
+import axios from "axios";
+import userPhoto from "../../assets/images/images.png"
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg ",
-                    followed: false,
-                    fullName: "Leha",
-                    status: "I am a boss of the GYM",
-                    location: {city: "Moscow", country: "Russia"}
-                },
-                {
-                    id: 2,
-                    photoUrl: "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg",
-                    followed: true,
-                    fullName: "Dmitry",
-                    status: "slave",
-                    location: {city: "Minsk", country: "Belarus"}
-                },
-                {
-                    id: 3,
-                    photoUrl: "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg",
-                    followed: true,
-                    fullName: "Andrew",
-                    status: "slave",
-                    location: {city: "Belgorod", country: "Russia"}
-                },
-                {
-                    id: 4,
-                    photoUrl: "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg",
-                    followed: false,
-                    fullName: "Lena",
-                    status: "slave",
-                    location: {city: "Vorkuta", country: "Russia"}
-                }
-                ]
-        )
-    }
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>
+            props.setUsers(response.data.items))
+        };
     return (<div>
         {props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={s.userPhoto}/>
+                            <img src={u.photos.small !=null ? u.photos.small:userPhoto} className={s.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
@@ -54,12 +21,12 @@ let Users = (props) => {
                     </span>
             <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
         </div>)}
