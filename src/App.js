190b709bd19127cce3b219c200fdfa0fbe97a342
Yelkira <penda.fu@gmail.com>
2022-11-09 20:56:59 +0300

    61 login

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ const App = (props) => {
                 <NavBar/>
                 <div className="app-wrapper-content">
                     <Routes>
+                        <Route path="/profile/:userId"
+                               element={<ProfileContainer/>}/>
                         <Route path="/profile/*"
                                element={<ProfileContainer/>}/>
                         <Route path="/dialogs/*"
